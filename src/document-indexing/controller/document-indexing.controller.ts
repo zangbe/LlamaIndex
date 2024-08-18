830aa6a0cab1b93d7ad483078d705a6c3ab10914
zangbe <zangbe@naver.com>
2024-08-18 14:31:40 +0900

    add exception filter

--- src/document-indexing/controller/document-indexing.controller.ts
+++ src/document-indexing/controller/document-indexing.controller.ts
@@ -1,31 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { DocumentIndexingService } from '../application/document-indexing.service.js';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { QueryDocumentDto } from '../application/dto/query-document.dto.js';
+import { Body, Controller, Post } from "@nestjs/common";
+import { DocumentIndexingService } from "../application/document-indexing.service.js";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { QueryDocumentDto } from "../application/dto/query-document.dto.js";
 
-@ApiTags('document')
+@ApiTags("document")
 @Controller({
-  path: 'document',
+  path: "document",
 })
 export class DocumentIndexingController {
   constructor(private readonly service: DocumentIndexingService) {}
 
-  @Post('/query')
+  @Post("/query")
   @ApiOperation({
-    summary: 'Query the query engine and get a response.',
+    summary: "Query the query engine and get a response.",
     description:
-      'A query engine that uses a retriever to query an index and then synthesizes the response.',
+      "A query engine that uses a retriever to query an index and then synthesizes the response.",
   })
   @ApiResponse({ status: 201, type: String })
   async query(@Body() dto: QueryDocumentDto) {
     return this.service.query(dto.text);
   }
 
-  @Post('/multi-turn')
+  @Post("/multi-turn")
   @ApiOperation({
     summary: `Send message along with the class's current chat history to the LLM.`,
     description:
-      'ContextChatEngine uses the Index to get the appropriate context for each query',
+      "ContextChatEngine uses the Index to get the appropriate context for each query",
   })
   @ApiResponse({ status: 201, type: String })
   async chat(@Body() dto: QueryDocumentDto) {
